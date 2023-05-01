c9d285ba41734823d0119cd4ff7d5a50388bd837
unknown <Monirul-Jim>
2023-05-01 23:53:08 +0600

    hero section

--- src/layout/homePage/HomePage.jsx
+++ src/layout/homePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../../pages/Header/Header';
 
 const HomePage = () => {
     return (
-        <div>
-            this is home page
-        </div>
+        <>
+           <Header></Header>
+        </>
     );
 };
 
--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import foodPic from '../../assets/food picture.jpg'
 
 const Header = () => {
     return (
-        <div>
+        <>
+            <div className="relative h-screen overflow-hidden">
+                <img className="absolute top-0 left-0 w-full h-full object-cover" src={foodPic} alt="" />
+                <div className="absolute inset-0 bg-black opacity-50"></div>
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <div className="text-center">
+                        <h1 className="text-3xl sm:text-5xl text-white font-bold leading-tight mb-6">Welcome<br /> To the Hunger<br /> Restaurants</h1>
+                        <h2 className="text-xl sm:text-3xl text-white font-medium leading-tight">Experience The Best Food In The Country</h2>
+                    </div>
+                </div>
+            </div>
             
-        </div>
+            </>
+
     );
 };
 
