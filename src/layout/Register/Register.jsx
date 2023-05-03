1dc48f308f2f6ad5144e806d5ec87265866ea21f
unknown <Monirul-Jim>
2023-05-03 13:06:25 +0600

    provider

--- src/layout/Register/Register.jsx
+++ src/layout/Register/Register.jsx
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NavigationBar from '../../shared/navigationBar/NavigationBar';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../provider/AuthProvider';
 
 const Register = () => {
+    const {registerUser}=useContext(AuthContext)
+    
+    const handleRegister=(event)=>{
+        event.preventDefault();
+        const form=event.target;
+        const name=form.name.value;
+        const email=form.email.value;
+        const password=form.password.value
+        console.log(name,email,password);
+    }
     return (
         <div>
             <NavigationBar></NavigationBar>
-            <div className="max-w-md mx-auto mt-10">
-                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
-                            Username
-                        </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+            <h1 className='text-blue-900 text-2xl text-center mt-4'>Please Register</h1>
+            <div  className="hero min-h-screen bg-base-200">
+                {/* <div className="hero-content flex-col lg:flex-row-reverse"> */}
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input name='Name' type="text" placeholder="name" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input name='Email' type="email" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input name='Password' type="password" placeholder="password" className="input input-bordered" />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button onClick={handleRegister} className="btn btn-primary">Register</button>
+                            </div>
+                            <p>Don't have an account please ? <Link className='text-blue-800 font-semibold underline' to='/login'>Login</Link></p>
+                        </div>
                     </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
-                            Password
-                        </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********" />
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                           Register
-                        </button>
-                    </div>
-                    <p>Already have an account please ? <Link className='text-blue-800 font-semibold underline' to='/login'>Login</Link></p>
-                </form>
+                {/* </div> */}
             </div>
-
         </div>
     );
 };
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,21 +1,23 @@
 import React, { createContext } from 'react';
-import {getAuth} from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import app from '../../firebase.config.init';
 
 
-export const AuthContext=createContext(null)
-const auth=getAuth(app)
+export const AuthContext = createContext(null)
+const auth = getAuth(app)
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
 
-
-    const authInfo={
-
+    const registerUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+    const authInfo = {
+        registerUser
     }
     return (
-       <AuthContext.Provider value={authInfo}>
-        {children}
-       </AuthContext.Provider>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
