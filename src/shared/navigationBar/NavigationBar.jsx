12aa3750c2eafb4d6bd6118d864cf1f95b22e13b
unknown <Monirul-Jim>
2023-05-03 19:55:58 +0600

    activelink

--- src/shared/navigationBar/NavigationBar.jsx
+++ src/shared/navigationBar/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../provider/AuthProvider';
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const NavigationBar = () => {
     const {user,logOut,updatePic}=useContext(AuthContext)
@@ -10,7 +11,7 @@ const NavigationBar = () => {
         .catch(error=>console.log(error))
     }
     return (
-        <div className="navbar bg-base-100">
+            <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -18,27 +19,25 @@ const NavigationBar = () => {
                     </label>
                     <ul tabIndex={0} className="menu menu-compact  dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         <Link to='/'>Home</Link>
-
+                
                         <Link to='/blog-page'>Blog</Link>
-                        <Link>User</Link>
                     </ul>
                 </div>
                 <Link to='/' className="btn btn-ghost normal-case text-xl" >Hungry Monster</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 space-x-6">
-                    <Link to='/'>Home</Link>
-                    <Link to='/blog-page'>Blog</Link>
-                    <Link>User</Link>
+                  <ActiveLink to='/'>Home</ActiveLink>
+                    <ActiveLink to='/blog-page'>Blog</ActiveLink>
                 </ul>
             </div>
             <div className="navbar-end">
                 {
-                    user &&  <img  className='h-12 w-12 rounded-lg rounded-full' src={user?.photoURL} alt="Profile Picture" />
+                    user &&  <img title={user.email}  className='h-12 w-12 rounded-full' src={user?.photoURL} alt="Profile Picture" />
                 }
                 
                {
-                user ?  <Link><button onClick={handleLogout} className="btn btn-primary btn-sm">Logout</button></Link> :
+                user ?  <Link><button onClick={handleLogout} className="btn ms-2 btn-primary btn-sm">Logout</button></Link> :
                 <Link to='/login'><button className="btn btn-primary btn-sm">Login</button></Link>
                }
             </div>
